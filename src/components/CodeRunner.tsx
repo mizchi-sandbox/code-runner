2a4ad6dc639371e5c5f96765b862ac1097a45878
mizchi <miz404@gmail.com>
2019-03-03 19:16:37 +0900

    Refactor

---
+++ src/components/CodeRunner.tsx
@@ -0,0 +1,89 @@
+import * as ts from "typescript";
+import { compile } from "../lib/compileMarkdown";
+import React, { useRef, useLayoutEffect } from "react";
+import { readFileSync } from "fs";
+
+import { bundle } from "../lib/bundle";
+
+const ReactSource = readFileSync(
+  __dirname + "/../../node_modules/react/umd/react.development.js"
+).toString();
+
+const ReactDOMSource = readFileSync(
+  __dirname + "/../../node_modules/react-dom/umd/react-dom.development.js"
+).toString();
+
+export function CodeRunner(props: { value: string }) {
+  const ref = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    (async () => {
+      if (ref.current) {
+        const ret = compile(props.value);
+        const codeMap = ret.codeblocks.reduce((acc: object, i: any) => {
+          const [filename, code] = sourceToCode(i.value);
+          return { ...acc, [filename]: code };
+        }, {});
+        let html = "";
+        try {
+          const code = await bundle(codeMap);
+          html = `
+          <div class="root"></div>
+          <script>
+            ${ReactSource};
+            ${ReactDOMSource};
+            ${code};
+          </script>
+        `;
+        } catch (e) {
+          console.error(e);
+          html = `<div style="color: red;">${e.toString()}</div>`;
+        }
+        const iframe = createIframe(html);
+        iframe.style.width = "50vw";
+        iframe.style.height = "calc(80vh)";
+        ref.current.innerHTML = "";
+        ref.current.appendChild(iframe);
+      }
+    })();
+  }, [props.value]);
+  return (
+    <div>
+      <div ref={ref} />
+    </div>
+  );
+}
+
+function compileTypeScript(value: string): string {
+  return ts.transpileModule(value, {
+    compilerOptions: {
+      module: ts.ModuleKind.ES2015,
+      jsx: ts.JsxEmit.React
+      // target: "ES2017"
+    }
+  }).outputText;
+}
+
+function sourceToCode(value: string) {
+  const [first] = value.split("\n");
+  const match = first.match(/\/\/\sfile\:([\w]+\.(js|ts|jsx|tsx))/);
+  const code = compileTypeScript(value);
+  if (match != null) {
+    const file = match[1];
+    if (["index.js", "index.ts", "index.tsx"].includes(file)) {
+      return ["index.js", code];
+    } else {
+      const fileName = file.replace(/\.(js|ts|tsx)$/, "");
+      const relFileName = fileName[0] != "." ? `./${fileName}` : fileName;
+      return [relFileName, code];
+    }
+  } else {
+    return ["index.js", code];
+  }
+}
+
+function createIframe(html: string) {
+  const iframe: any = document.createElement("iframe");
+  iframe.sandbox = "allow-scripts";
+  iframe.src = URL.createObjectURL(new Blob([html], { type: "text/html" }));
+  return iframe;
+}
---
+++ src/lib/bundle.tsx
@@ -0,0 +1,65 @@
+import path from "path";
+import * as rollup from "rollup";
+
+// TODO: import js
+export async function bundle(fileMap: object) {
+  const bundle = await rollup.rollup({
+    input: "index.js",
+    plugins: [
+      virtual({
+        "index.js": "",
+        ...fileMap,
+        react: "export default window.React",
+        "react-dom": "export default window.ReactDOM"
+      })
+    ]
+  });
+  const gen = await bundle.generate({
+    format: "iife",
+    sourcemap: false,
+    name: "window",
+    extend: true
+  });
+  // eval
+  const code = gen.output[0].code as string;
+  return code;
+}
+
+// plugin
+
+const PREFIX = `\0virtual:`;
+
+function virtual(modules: any): rollup.Plugin {
+  const resolvedIds = new Map();
+
+  Object.keys(modules).forEach(id => {
+    resolvedIds.set(path.resolve(id), modules[id]);
+  });
+
+  return {
+    name: "virtual",
+
+    resolveId(id, importer) {
+      if (id in modules) {
+        return PREFIX + id;
+      }
+
+      if (importer) {
+        if (importer.startsWith(PREFIX))
+          importer = importer.slice(PREFIX.length);
+        const resolved = path.resolve(path.dirname(importer), id);
+        if (resolvedIds.has(resolved)) {
+          return PREFIX + resolved;
+        }
+      }
+    },
+
+    load(id: string) {
+      if (id.startsWith(PREFIX)) {
+        id = id.slice(PREFIX.length);
+
+        return id in modules ? modules[id] : resolvedIds.get(id);
+      }
+    }
+  };
+}
